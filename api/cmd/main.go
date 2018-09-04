f1fb5422d71ce8b413a289110ce4e9c69286f74b
root <zengshufang@huawei.com>
2018-09-04 17:11:17 +0800

    Add APIs implementationof policy, connector and plan

--- api/cmd/main.go
+++ api/cmd/main.go
@@ -41,7 +41,29 @@ func main() {
 	ws.Route(ws.GET("/policies/{name}").To(handler.GetPolicy)).
 		Doc("Get policy details")
 	ws.Route(ws.POST("/policies/{name}").To(handler.CreatePolicy)).
-		Doc("Create policy details")
+		Doc("Create policy")
+	ws.Route(ws.PUT("/policies/{id}").To(handler.UpdatePolicy)).
+		Doc("Update policy")
+	ws.Route(ws.DELETE("/policies/{id}").To(handler.DeletePolicy)).
+		Doc("Delete policy")
+
+	ws.Route(ws.GET("/connector/{name}").To(handler.GetConnector)).
+		Doc("Get connector details")
+	ws.Route(ws.POST("/connector/{name}").To(handler.CreateConnector)).
+		Doc("Create connector")
+	ws.Route(ws.PUT("/connector/{id}").To(handler.UpdateConnector)).
+		Doc("Update connector")
+	ws.Route(ws.DELETE("/connector/{id}").To(handler.DeleteConnector)).
+		Doc("Delete connector")
+
+	ws.Route(ws.GET("/plan/{name}").To(handler.GetPlan)).
+		Doc("Get plan details")
+	ws.Route(ws.POST("/plan/{name}").To(handler.CreatePlan)).
+		Doc("Create plan")
+	ws.Route(ws.PUT("/plan/{id}").To(handler.UpdatePlan)).
+		Doc("Update plan")
+	ws.Route(ws.DELETE("/plan/{id}").To(handler.DeletePlan)).
+		Doc("Delete plan")
 
 	wc.Add(ws)
 	webService.Handle("/", wc)
